c99c9c0f396f3b9916fee95077d88498c7c9ca7e
RockfordWei <rocky@perfect.org>
2018-01-16 15:35:40 -0500

    Upgrade to Swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,20 +1,10 @@
-// Generated automatically by Perfect Assistant Application
-// Date: 2017-06-02 20:23:17 +0000
+import Foundation
 import PackageDescription
 let package = Package(
-	name: "APIDocumentationServer",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 1),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-SMTP", majorVersion: 1),
-		.Package(url: "https://github.com/iamjono/SwiftMoment.git", majorVersion: 0),
-		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 0),
-		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", majorVersion: 1),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-LocalAuthentication-PostgreSQL.git", majorVersion: 1),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Markdown.git", majorVersion: 1),
-	]
+		name: "APIDocumentationServer",
+		targets: [],
+		dependencies: [
+			.Package(url: "https://github.com/PerfectlySoft/Perfect-LocalAuthentication-PostgreSQL.git", majorVersion: 3),
+			.Package(url: "https://github.com/PerfectlySoft/Perfect-Markdown.git", majorVersion: 3),
+		]
 )
